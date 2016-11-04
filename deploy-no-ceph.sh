37e2051ac7ddb15aa0809ea29c73c4e0797202df
Kevin Jones <kevinjones@redhat.com>
2016-11-04 16:20:19 -0400

    Adding a non-ceph deployment
    Signed-off-by: Kevin Jones <kevinjones@redhat.com>

--- deploy-no-ceph.sh
+++ deploy-no-ceph.sh
@@ -3,20 +3,15 @@
 cd ~
 source stackrc
 
-#--neutron-bridge-mappings datacentre:br-ex,tenant:br-tenant,storage:br-storage \
-#--neutron-network-vlan-ranges datacentre:1:1000,tenant:1:1000,storage:1:1000 \
-
 openstack overcloud deploy --templates \
-	-e /home/stack/templates/storage-environment-ceph.yaml \
+        -e /home/stack/templates/storage-environment-local.yaml \
 	-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 	-e /home/stack/templates/network-environment.yaml \
 	-e /home/stack/templates/firstboot-environment.yaml \
 	--control-flavor control \
 	--compute-flavor compute \
-	--ceph-storage-flavor ceph-storage \
-	--control-scale 3 \
-	--compute-scale 3 \
-	--ceph-storage-scale 3 \
+	--control-scale 1 \
+	--compute-scale 1 \
         --libvirt-type qemu \
 	--ntp-server clock.redhat.corp \
 	--neutron-tunnel-types vxlan \
